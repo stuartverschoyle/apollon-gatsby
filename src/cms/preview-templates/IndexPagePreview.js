9f992b6738aa1fad8180c44a8bb276ac02d414ae
stuart verschoyle <stuart_verschoyle@hotmail.co.uk>
2020-11-06 12:12:25 +0000

    page preview

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
-import remark from 'remark'
-import remarkHTML from 'remark-html'
-
-const toHTML = value => remark()
-                            .use(remarkHTML)
-                            .processSync(value)
-                            .toString()
 
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
@@ -20,7 +13,7 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         heading={data.heading}
         intro={data.intro || { blurbs: [] }}
         featuretitle={data.featuretitle}
-        mainpitch={toHTML(data.mainpitch.title) || toHTML(data.mainpitch.description) || toHTML(data.mainpitch.subtitle) || toHTML(data.mainpitch.subdescription)}
+        mainpitch={data.mainpitch.title || data.mainpitch.description || data.mainpitch.subtitle || data.mainpitch.subdescription}
         main={data.main || {}}
         carousel={data.carousel || { slides: [] }}
         products={data.products || { slides: [] }}
