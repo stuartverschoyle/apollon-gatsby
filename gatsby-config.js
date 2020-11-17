c8f27e1250fa94e57d1a06580608e4cc708eca8f
stuart verschoyle <stuart_verschoyle@hotmail.co.uk>
2020-11-17 19:31:52 +0000

    change og image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Apollon Formularies U.K',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Combining pioneering science with the power of nature.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`${withPrefix('/')}img/Hero_image@2x.png`}
         />
       </Helmet>
       <Navbar />
